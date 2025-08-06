83735e57d05b15caa8451085f7a06970bdac44d6
Carlos3du <carlos.mec2005@gmail.com>
2025-08-06 17:25:12 -0300

    add: eda

--- src/tokenizing.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoTokenizer
-
-def config_tokenizer(model, df, col_name, padding="longest", truncation=True, max_length=512):
-    tokenizer = AutoTokenizer.from_pretrained(model)
-    return tokenizer(df[col_name], padding=padding, truncation=truncation, max_length=max_length)
